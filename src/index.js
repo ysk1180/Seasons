a1b94a3fefe19012b891b8d60ef476ed9f296a3b
Yusuke Miyazaki <ysk1180@gmail.com>
2019-06-22 09:27:03 +0900

    hooksシステムを使ってリファクタリング

--- src/index.js
+++ src/index.js
@@ -1,38 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, errorMessage] = useLocation();
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="please accept location request" />
   }
 
-  renderContent(){
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-
-    return <Spinner message="please accept location request" />
-  }
-
-  render() {
-    return (
-      <div className="border red">
-        {this.renderContent()}
-      </div>
-    );
-  }
-}
+  return <div className="border red">{content}</div>
+};
 
 ReactDOM.render(
   <App />,
